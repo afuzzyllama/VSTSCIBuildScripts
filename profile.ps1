5dabe1466a1e6dc641e1cc02ce13bf323af8d61e
afuzzyllama <afuzzyllama@users.noreply.github.com>
2016-02-14 17:58:10 -0500

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -2,4 +2,4 @@ $env:path += ";" + (Get-Item "Env:ProgramFiles").Value + "\Git\usr\bin\"
 $env:path += ";" + (Get-Item "Env:ProgramFiles").Value + "\Git\cmd\"
 $env:GIT_SSH_COMMAND = "ssh -o StrictHostKeyChecking=no"
 
-. 'C:\agent\posh-git-0.4\profile.ps1'
+. 'C:\agent\posh-git-0.4\profile.ps1' 2>&1 | Out-Null
