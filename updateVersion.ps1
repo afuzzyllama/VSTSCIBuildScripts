ab05a403a55c1adcd894b9df6b23d06c7d1dad8c
afuzzyllama <afuzzyllama@users.noreply.github.com>
2016-02-19 16:51:45 -0500

    Update updateVersion.ps1

--- updateVersion.ps1
+++ updateVersion.ps1
@@ -8,10 +8,12 @@ $assemblyInfoVersion = [regex]::Matches($assemblyInfoVersionString, "[0-9]+\.[0-
 $assemblyInfoVersion = [string]$assemblyInfoVersion[0]
 
 $assemblyVersion = [string]::Format("{0}.{1}", $assemblyInfoVersion, $env:BUILD_BUILDID)
-$AssemblyInformationalVersion = [string]::Format("{0}.{1}.{2}", $assemblyInfoVersion, $env:BUILD_BUILDID, $env:BUILD_SOURCEVERSION.SubString(0,7))
+$assemblyInformationalVersion = [string]::Format("{0}.{1}.{2}", $assemblyInfoVersion, $env:BUILD_BUILDID, $env:BUILD_SOURCEVERSION.SubString(0,7))
 
 $assemblyInfo = [regex]::Replace($assemblyInfo, "\[assembly: AssemblyVersion\(`"[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+`"\)\]", "[assembly: AssemblyVersion(`"$assemblyVersion`")]")
 $assemblyInfo = [regex]::Replace($assemblyInfo, "\[assembly: AssemblyFileVersion\(`"[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+`"\)\]", "[assembly: AssemblyFileVersion(`"$assemblyVersion`")]")
 
 $assemblyInfo | Out-File $assemblyInfoFile
-Add-Content $assemblyInfoFile "`n[assembly: AssemblyInformationalVersion(`"$AssemblyInformationalVersion`")]"
+Add-Content $assemblyInfoFile "`n[assembly: AssemblyInformationalVersion(`"$assemblyInformationalVersion`")]"
+
+Write-Host "Building version: $assemblyInformationalVersion" 
