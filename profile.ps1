a4040d27f09763b105d050c6ec9248bb66653d48
afuzzyllama <afuzzyllama@users.noreply.github.com>
2016-02-23 17:42:58 -0500

    No longer used in pipeline

--- profile.ps1
+++
@@ -1,5 +0,0 @@
-$env:path += ";" + (Get-Item "Env:ProgramFiles").Value + "\Git\usr\bin\"
-$env:path += ";" + (Get-Item "Env:ProgramFiles").Value + "\Git\cmd\"
-$env:GIT_SSH_COMMAND = "ssh -o StrictHostKeyChecking=no"
-
-. 'C:\agent\posh-git\0.4\profile.ps1' 2>&1 | Out-Null
