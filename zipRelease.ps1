ba50997ce26ec57fa90c32dace1dde642d40b777
afuzzyllama <afuzzyllama@users.noreply.github.com>
2016-02-27 11:14:59 -0500

    Update zipRelease.ps1

--- zipRelease.ps1
+++ zipRelease.ps1
@@ -8,8 +8,9 @@
     
     Requires the following custom environment variables to be set in TFS:
     
-        BuildScriptDir             - Absolute path to where all powershell scripts for this agent are stored
-        AssemblyInfoRelativePaths  - Relative path to the AssemblyInfo.cs file in the git repository
+        BuildScriptDir              - Absolute path to where all powershell scripts for this agent are stored
+        AssemblyInfoRelativePathss  - Relative paths to the AssemblyInfo.cs files in the git repository.  The paths should be comma delimited and the 
+                                      assembilies need to have the identical major and minor versions set.
 #>
 
 . "$env:BUILDSCRIPTDIR\sharedFunctions.ps1"
